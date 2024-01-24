bac37d04a443c6c46775cedc13149f33b7eaf2f4
Evgeny Petrov <petroves88@mail.ru>
2024-01-24 15:57:17 +0300

    refactor lesson2

--- lesson2/lesson2/src/main/java/ru/petrov/lesson2/Lesson2Application.java
+++ lesson2/lesson2/src/main/java/ru/petrov/lesson2/Lesson2Application.java
@@ -2,8 +2,11 @@ package ru.petrov.lesson2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import ru.petrov.lesson2.utils.SqlQwery;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackageClasses = SqlQwery.class)
 public class Lesson2Application {
 
     public static void main(String[] args) {
--- lesson2/lesson2/src/main/java/ru/petrov/lesson2/controller/UserController.java
+++ lesson2/lesson2/src/main/java/ru/petrov/lesson2/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.petrov.lesson2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,54 +13,60 @@ import ru.petrov.lesson2.service.UserService;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/users")
     public String findAll(Model model) {
         List<User> users = userService.findAll();
-
-
         model.addAttribute("users", users);
+        log.info("controller called findAll");
         return "user-list";
     }
 
     @GetMapping("/user-create")
     public String createUserForm(User user) {
+        log.info("controller called updateUserForm");
         return "user-create";
     }
 
     @GetMapping("/user-update/{id}")
     public String updateUserForm(User user) {
-        return "user-update";
+        log.info("controller called updateUserForm");
+
+        return
+                "user-update";
     }
 
     @PostMapping("/user-create")
     public String createUser(User user) {
         userService.saveUser(user);
+        log.info("controller called createUser");
         return "redirect:/users";
     }
 
     @GetMapping("/user-delete/{id}")
     public String deleteUser(@PathVariable Integer id) {
         userService.deleteUserById(id);
+        log.info("controller called deleteUser");
         return "redirect:/users";
     }
 
     @GetMapping("/user/{id}")
-    public String getUserById(@PathVariable Integer id,Model model){
-        model.addAttribute("user",userService.getUserById(id));
+    public String getUserById(@PathVariable Integer id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        log.info("controller called getUserById");
         return "user";
     }
 
     @PostMapping("/user-update")
-    public String updateUser(User user){
+    public String updateUser(User user) {
         userService.updateUser(user);
+        log.info("controller called updateUser");
         return "redirect:/users";
     }
 }
