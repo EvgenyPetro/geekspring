ba3d6980edb7235f6122e3bbd39f90e0968662eb
Evgeny Petrov <petroves88@mail.ru>
2024-02-19 19:08:46 +0300

    lesson11

---
+++ lesson11/metricsdemo/src/main/java/ru/petov/metricsdemo/MetricsdemoApplication.java
@@ -0,0 +1,13 @@
+package ru.petov.metricsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsdemoApplication.class, args);
+    }
+
+}
---
+++ lesson11/metricsdemo/src/test/java/ru/petov/metricsdemo/MetricsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petov.metricsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
